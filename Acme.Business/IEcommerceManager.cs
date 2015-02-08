8962e84e5f7df2f895e83cec035a246c66e53077
Parag Nair <parag.nair.dev@gmail.com>
2015-02-09 04:27:16 +0530

    Added interface and concrete class implementing the interface for a dummy ecommerce platform

---
+++ Acme.Business/IEcommerceManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Business
+{
+    public interface IEcommerceManager
+    {
+        string AddToBasket(string basketID, string productSKU, int quantity);
+    }
+}
