57f9bb9ed518472fb4514016ca02625262797f58
Parag Nair <parag.nair.dev@gmail.com>
2015-02-09 04:27:49 +0530

    Custom attribute to extend NUnit testing attributes

---
+++ UnitTestingFramework/CustomAttributes.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestingFramework
+{
+    public class GivenAttribute : TestFixtureSetUpAttribute { }
+
+    public class WhenAttribute : GivenAttribute { }
+
+    public class ThenAttribute : TestAttribute { }
+}
